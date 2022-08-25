78a2f89a27e483d6385971920a0f325ef527dbe0
Alina Pykus <alinapykus17@email.com>
2022-08-26 00:43:43 +0300

    add some changes

---
+++ main.js
@@ -0,0 +1,64 @@
+// task 1
+// function upperCase(string) {
+//     reg = ^[A-Z]/$;
+//     if (reg.test(string)) {
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
+// upperCase();
+
+// Task2
+//  function checkEmail(string) {
+//     let regexp = /\w+@\w+/;
+//     if (regexp.test(string)) {
+//         return true
+//     } else {
+//         return false
+//     }
+//  }
+
+ // Task3
+//  let string = "cdbBdbsbz";
+// string.exec(\w)
+
+// Task 4
+// let string = "Java Script";
+// function func() {
+//     let reg = /^JavaScript$/;
+//   string.test(reg.reverse());
+// }
+// func();
+// task 5
+// function number(cardNo) {
+//     let cardReg= (\d\d\d\d\-\d\d\d\d\-\d\d\d\d\-\d\d\d\d$\)
+
+//     if (cardNo.match(cardReg));
+//     return true;
+// } else {
+//     return false;
+// }
+
+
+
+// Task 6
+// function str(string) {
+//     reg = [^'0-10,a-zA-Z'$];
+//     if(string ===  reg) {
+//     alert('email is correct');
+//     } else {
+//         alert('somothing goes worng');
+//     }
+// }
+
+
+// Task 7
+// let log = prompt("login");
+//  function login() {
+//     reg = [2-10,a-zA-Z ?];
+
+//     log.test(reg);
+//   console.log('done')
+//  }
+//  reg();
